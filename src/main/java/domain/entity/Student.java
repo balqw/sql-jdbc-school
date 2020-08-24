f91174cd27209c4d50fa6ced45f6d6b95f44c7e8
balqw <balqw777@gmail.com>
2020-08-24 12:47:52 +0300

    add dao

---
+++ src/main/java/domain/entity/Student.java
@@ -0,0 +1,46 @@
+package domain;
+
+public class Student {
+    private int id;
+    private String first_name;
+    private String last_name;
+    private int group_id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public int getGroup_id() {
+        return group_id;
+    }
+
+    public void setGroup_id(int group_id) {
+        this.group_id = group_id;
+    }
+
+    public Student(int id, String first_name, String last_name) {
+        this.id = id;
+        this.first_name = first_name;
+        this.last_name = last_name;
+    }
+}
